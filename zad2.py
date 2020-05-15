2a84f1d1090dccce589f4cae7e04f3a563e5ddb9
KrzysztofWysocki <krzysiek27-052@o2.pl>
2020-05-15 19:20:16 +0200

    update

--- zad2.py
+++ zad2.py
@@ -1,15 +1,16 @@
-def createMatrix(rowCount, colCount, dataList):   
-    mat = [i,j]
-    for i in range (rowCount):
+def createMatrix(rowCount, colCount, dataList):
+    mat = []
+    for i in range(rowCount):
         rowList = []
-        for j in range (colCount):
-            if dataList[j] not in mat:
-                rowList.append(dataList[j])
+        for j in range(colCount):
+            rowList.append(dataList[rowCount * i + j])
         mat.append(rowList)
 
-    return mat 
+    return mat
 
-def main():  
+def main():
     alpha = ['a','b','c','d','e','f','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     mat = createMatrix(5,5,alpha)
     print (mat)
+
+main()
